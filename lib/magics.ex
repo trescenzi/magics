f294b0f4cc4258832b009f78b06ecf7c9aa26407
Thomas <trescenzi@gmail.com>
2015-07-01 23:18:58 -0400

    feat(modern): make everything pretty and parallel

--- lib/magics.ex
+++ lib/magics.ex
@@ -4,16 +4,30 @@ defmodule Magics do
   end 
 
   def get_card_names_at_page(page) do
-    response_body = get_top_cards_at_page(1).body
+    response_body = get_top_cards_at_page(page).body
     list_of_names = Floki.parse(response_body)
                     |> Floki.find(".L14") 
                     |> Enum.take_every(3) 
                     |> Enum.map(fn(x) -> elem(x,2) end)
     List.flatten list_of_names
   end
-  def modern_most_played_cards do
-    Enum.reduce(1..20, [], fn(page_num, list_of_names) ->
-      list_of_names ++ get_card_names_at_page(page_num)  
+
+  def parallel_card_fetch do
+    list_of_names = []
+    me = self
+    Enum.map( 1..20, fn(page_num) ->
+      spawn_link fn -> 
+        send(me, {self, get_card_names_at_page page_num})
+      end
     end)
+    |> Enum.map( fn(pid) ->
+      receive do {^pid, names} -> 
+        list_of_names ++ names
+      end
+    end)
+  end
+
+  def most_played_modern_cards do
+    List.flatten parallel_card_fetch
   end
 end
